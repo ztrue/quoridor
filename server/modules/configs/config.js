458d8a71e47543beef5a4c7241cce290bc85058d
Anton Grinin <gztrue@gmail.com>
2013-09-08 20:38:08 +0400

    Use local config only if it exists

--- server/modules/configs/config.js
+++ server/modules/configs/config.js
@@ -1,14 +1,18 @@
+var fs = require('fs');
+
 exports.PORT = 80;
 
 exports.LOGGER = '';
 
 exports.PUBLIC = PATH_BIN; // PATH_BUILD for dev
 
-var configLocal = require(PATH_CONFIGS + 'config.local');
+if (fs.existsSync(PATH_CONFIGS + 'config.local')) {
+  var configLocal = require(PATH_CONFIGS + 'config.local');
 
-// rewrite default config with local config
-for (var param in this) {
-  if (configLocal[param] !== undefined) {
-    this[param] = configLocal[param];
+  // rewrite default config with local config
+  for (var param in this) {
+    if (configLocal[param] !== undefined) {
+      this[param] = configLocal[param];
+    }
   }
 }
